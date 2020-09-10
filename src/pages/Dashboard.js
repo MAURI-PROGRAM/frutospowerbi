a0a1d1e679480d757c471f0e4241ed305248ef07
Michael Merchan <michaelmerchan@Mac-mini-de-Michael.local>
2020-09-09 22:41:13 -0500

    se arreglaron bugs

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Dashboard">
-           <iframe  width="1200" height="1000" src="https://app.powerbi.com/view?r=eyJrIjoiNGJmMjY0YjAtNGY2YS00MmVkLTkyNDAtMzZkNTI5YzNjOWU3IiwidCI6IjI1MGY3NmU3LTYxMDUtNDJlMy04MmQwLWJlN2M0NjBhZWE1OSIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+           <iframe title="Dashboard ventas"  width="1200" height="1000" src="https://app.powerbi.com/view?r=eyJrIjoiNGJmMjY0YjAtNGY2YS00MmVkLTkyNDAtMzZkNTI5YzNjOWU3IiwidCI6IjI1MGY3NmU3LTYxMDUtNDJlMy04MmQwLWJlN2M0NjBhZWE1OSIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
            </div>
     );
   }
